154351c960ed5307706c9a7a8992870debdc17f5
harperrose <101375619+harperrose@users.noreply.github.com>
2026-04-29 10:55:44 -0400

    dashboard changes

--- content.js
+++ content.js
@@ -1,20 +1,27 @@
 const SITE_DATA = {
+    // Links for the nav and ads
     links: {
         twitter: "https://x.com/yourprofile",
-        youtube: "https://www.youtube.com/@ooukfunkyoo",
-        shop: "/shop",
-        guestbook: "/api/comments" // Or a 3rd party link
+        youtube: "https://youtube.com/c/yourchannel",
+        shop: "#",
+        privacy: "#"
     },
-    ads: {
-        gifAdUrl: "https://media.giphy.com/media/example/giphy.gif",
-        buySemLink: "#buy"
-    }
+    // Images for the 1/7 Carousel
+    carousel: [
+        "https://picsum.photos/id/10/200/300",
+        "https://picsum.photos/id/20/200/300",
+        "https://picsum.photos/id/30/200/300",
+        "https://picsum.photos/id/40/200/300",
+        "https://picsum.photos/id/50/200/300",
+        "https://picsum.photos/id/60/200/300",
+        "https://picsum.photos/id/70/200/300"
+    ]
 };
 
-// Function to inject content
-document.addEventListener("DOMContentLoaded", () => {
-    document.querySelectorAll('[data-content]').forEach(el => {
-        const key = el.getAttribute('data-content');
-        if (SITE_DATA.links[key]) el.href = SITE_DATA.links[key];
-    });
-});
+let currentImgIndex = 0;
+
+function updateCarousel(direction) {
+    currentImgIndex = (currentImgIndex + direction + SITE_DATA.carousel.length) % SITE_DATA.carousel.length;
+    document.getElementById('carousel-img').src = SITE_DATA.carousel[currentImgIndex];
+    document.getElementById('carousel-counter').innerText = `${currentImgIndex + 1}/7`;
+}
